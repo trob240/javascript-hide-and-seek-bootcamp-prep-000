79a0bff33cfeedbdcdf6153efba8b552ba16c95c
Trevahr Robertson <trevahr@yahoo.com>
2019-03-26 12:37:25 +0000

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ function getFirstSelector(selector)
  return document.querySelector(selector)
 }
 
+
+
 function nestedTarget()
 {
   
 }
+
+
+function increaseRankBy(n)
+{
+  
+}
+
+
+function deepestChild()
+{
+  
+}
+
